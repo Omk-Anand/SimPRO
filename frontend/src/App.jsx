55aa775a5a4bee04ec4c97c76ca8e54655a2cc2e
Omk-Anand <159501554+Omk-Anand@users.noreply.github.com>
2026-08-09 14:44:09 -0700

    Add Dashboard component to App

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,1 +1,12 @@
+import React from 'react';
+import Dashboard from './Dashboard';
 
+function App() {
+  return (
+    <div className="w-screen h-screen">
+      <Dashboard />
+    </div>
+  );
+}
+
+export default App;
